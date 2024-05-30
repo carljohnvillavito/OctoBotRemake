55e2933b68319221099c8f937b0be80b1974608f
Carl John <135079894+CjsPortfolio@users.noreply.github.com>
2024-05-31 06:34:24 +0800

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,54 +1,37 @@
-const { CronJob } = require('cron');
-const fs = require('fs');
-const path = require('path');
+const axios = require('axios');
 
-// Function to send a message to all threads
-function sendGreetingMessage(api, message) {
-    api.getThreadList(100, null, ["INBOX"], (err, list) => {
-        if (err) {
-            console.error('Error fetching thread list:', err);
-            return;
-        }
-        list.forEach(thread => {
-            api.sendMessage(message, thread.threadID, (err) => {
-                if (err) {
-                    console.error(`Error sending greeting message to thread ${thread.threadID}:`, err);
-                } else {
-                    console.log(`Greeting message sent to thread ${thread.threadID}`);
-                }
+function sendHourlyMessage(api, message) {
+    setInterval(() => {
+        api.getThreadList(100, null, ["INBOX"], (err, list) => {
+            if (err) {
+                console.error('Error fetching thread list:', err);
+                return;
+            }
+            list.forEach(thread => {
+                api.sendMessage(message, thread.threadID, (err) => {
+                    if (err) {
+                        console.error(`Error sending hourly message to thread ${thread.threadID}:`, err);
+                    } else {
+                        console.log(`Hourly message sent to thread ${thread.threadID}`);
+                    }
+                });
             });
         });
-    });
+    }, 29 * 60 * 1000); 
 }
 
 function init(api) {
-    const jobs = [
-        {
-            time: '0 0 6 * * *', // 6 am
-            message: 'Good Morning!'
-        },
-        {
-            time: '0 0 12 * * *', // 12 pm
-            message: 'Good Afternoon!'
-        },
-        {
-            time: '0 0 18 * * *', // 6 pm
-            message: 'Good Evening!'
-        },
-        {
-            time: '0 0 22 * * *', // 10 pm
-            message: 'Good Night!'
-        }
-    ];
 
-    jobs.forEach(job => {
-        new CronJob({
-            cronTime: job.time,
-            onTick: () => sendGreetingMessage(api, job.message),
-            start: true,
-            timeZone: 'Asia/Singapore' // GMT+8
-        });
-    });
+    const message = "Thank for using my bot^^. Will be adding future commands soon. Stay stuned^^";
+    
+    /* using fs 
+    const message  = {
+        body: `WELCOME TO YETANOTHERFBBOT`,
+        attachment: fs.createReadStream('cache/logo1.png')
+      };
+      */
+      
+    sendHourlyMessage(api, message);
 }
 
 module.exports = {
