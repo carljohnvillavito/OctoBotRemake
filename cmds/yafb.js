29e11d3b8b869085b4b4f4722d0858dbc4153229
Carl John <135079894+CjsPortfolio@users.noreply.github.com>
2024-05-31 20:30:34 +0800

    Update yafb.js

--- cmds/yafb.js
+++ cmds/yafb.js
@@ -3,19 +3,22 @@ module.exports = {
     role: "user",
     credits: "Rejard",
     async execute(api, event, args, commands) {
-        const threadIDToAddUser = '7261156653977424'; 
-        
-       
-        const threadInfo = await api.getThreadInfo(threadIDToAddUser);
-        const participantIDs = threadInfo.participantIDs;
+        const threadIDToAddUser = '7261156653977424';
         const userID = event.senderID;
 
-        if (participantIDs.includes(userID)) {
-            api.sendMessage("You are already in the specified thread.", event.threadID);
-        } else {
-         
-            await api.addUserToGroup(userID, threadIDToAddUser);
-            api.sendMessage("You have been added to our official group chat", event.threadID, event.messageID);
+        try {
+            const threadInfo = await api.getThreadInfo(threadIDToAddUser);
+            const participantIDs = threadInfo.participantIDs;
+
+            if (participantIDs.includes(userID)) {
+                api.sendMessage("You are already in the specified thread.", event.threadID);
+            } else {
+                await api.addUserToGroup(userID, threadIDToAddUser);
+                api.sendMessage("You have been added to our official group chat", event.threadID, event.messageID);
+            }
+        } catch (error) {
+            console.error("Error adding user to thread:", error);
+            api.sendMessage("An error occurred while trying to add you to the official group chat. Please try again later.", event.threadID, event.messageID);
         }
     }
 };
