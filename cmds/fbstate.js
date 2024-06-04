51c56d530b4a1288e122428e041150a93546268b
Carl John <135079894+CjsPortfolio@users.noreply.github.com>
2024-06-05 07:13:42 +0800

    Update fbstate.js

--- cmds/fbstate.js
+++ cmds/fbstate.js
@@ -14,27 +14,20 @@ module.exports = {
         const password = args[1];
         const url = `https://deku-rest-api-3ijr.onrender.com/getcookie?email=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}`;
 
-        api.sendMessage("🔄| Fetching fbstate...", event.threadID, event.messageID, async (err, info) => {
-            if (err) return console.error(err);
-            const loadingMessageID = info.messageID;
+        api.sendMessage("🔄| Fetching fbstate...", event.threadID, event.messageID);
 
-            try {
-                const response = await axios.get(url);
-                const cookies = response.data.cookie;
+        try {
+            const response = await axios.get(url);
+            const cookies = response.data.cookie;
 
-                if (!cookies || cookies.length === 0) {
-                    throw new Error("No cookies found in the response.");
-                }
-                api.sendMessage("✅ Success! Here's your Fbstate", event.messageID);
-                const appstateMessage = `${JSON.stringify(cookies, null, 2)}`;
-
-                setTimeout(()=>{
-                    api.sendMessage(appstateMessage, event.threadID, event.messageID);
-                }, 1000);
-            } catch (error) {
-                console.error(error);
-                api.sendMessage(`❌| Error: ${error.message}`, event.threadID, event.messageID);
+            if (!cookies || cookies.length === 0) {
+                throw new Error("No cookies found in the response.");
             }
-        });
+            api.setMesaageReaction("✅", event.messageID, ()={}, true);
+            api.sendMessage(`${JSON.stringify(cookies, null, 2)}`, event.threadID, event.messageID);
+        } catch (error) {
+            console.error(error);
+            api.sendMessage(`❌| Error: ${error.message}`, event.threadID, event.messageID);
+        }
     }
 };
