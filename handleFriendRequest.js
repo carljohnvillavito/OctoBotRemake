1c4bc4f8af61d8132a9497404dbe0f1918042cba
Carl John <135079894+CjsPortfolio@users.noreply.github.com>
2024-06-06 21:22:32 +0800

    Delete handleFriendRequest.js

--- handleFriendRequest.js
+++
@@ -1,7 +0,0 @@
-module.exports = function handleFriendRequest(api, event, accept = true, callback = () => {}) {
-    if (accept) {
-        api.handleFriendRequest(event.from, true, callback);
-    } else {
-        api.handleFriendRequest(event.from, false, callback);
-    }
-};
