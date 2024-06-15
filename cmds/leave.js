454b9017e280d3607a8bdc51ffca9303a78253de
Carl John <135079894+CjsPortfolio@users.noreply.github.com>
2024-06-15 14:29:00 +0800

    Update leave.js

--- cmds/leave.js
+++ cmds/leave.js
@@ -1,7 +1,7 @@
 module.exports = {
     description: "Bot leave the group",
-    role: "botadmin",
-    cooldown: 80,
+    role: "admin",
+    cooldown: 2,
     execute(api, event, args, commands) {
   try { 
   if (!args[0]) return api.removeUserFromGroup(api.getCurrentUserID(), event.threadID);
