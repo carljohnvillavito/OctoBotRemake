4348d79cd81a82ca4fb742c3dad8980c5572fd4e
Carl John B. Villavito <carljohnvillavito2023@gmail.com>
2024-06-25 21:39:21 +0800

    Update welcome.js

--- events/welcome.js
+++ events/welcome.js
@@ -21,11 +21,11 @@ module.exports = {
                         const memberCount = threadInfo.participantIDs.length;
 
                         // If the bot is added to the group
-                        api.sendMessage(`✅ Hello! This bot is now Online in ${groupName}\nMembers: ${memberCount}\n—————————————\nℹ️• Feel free to use it anytime!\nℹ️• 24/7 Active!\nℹ️• Owner: https://www.facebook.com/carljohn.villavito \nℹ️• Co-owner: https://www.facebook.com/61557924257806 \n—————————————`, event.threadID, async () => {
+                        api.sendMessage(`✅ Hello! This bot is now Online in ${groupName}\nMembers: ${memberCount}\n—————————————\nℹ️• Feel free to use it anytime!\nℹ️• 24/7 Active!\nℹ️• Owner: https://www.facebook.com/61554890228006\n—————————————`, event.threadID, async () => {
                             // Change the bot's nickname to the default
                             const botInfo = await api.getUserInfo(api.getCurrentUserID());
                             const firstName = botInfo[api.getCurrentUserID()].firstName;
-                            const defaultNickname = `${config.PREFIX} - ${firstName}-sama`;
+                            const defaultNickname = `[${config.PREFIX}] »⟩ ${firstName}`;
                             await api.changeNickname(defaultNickname, event.threadID, api.getCurrentUserID());
                         });
                     } else {
